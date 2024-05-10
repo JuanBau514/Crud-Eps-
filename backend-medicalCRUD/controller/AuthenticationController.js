a75136a8e76167071bf1ee340e49225c7f749e1b
Santiago Riano <santiagoriano68@gmail.com>
2024-05-09 22:32:26 -0500

    insertar usuario en la bd parte 1.php

--- backend-medicalCRUD/controller/AuthenticationController.js
+++ backend-medicalCRUD/controller/AuthenticationController.js
@@ -3,42 +3,46 @@ import Token from '../models/Token.js';
 import { hashPassword } from '../utils/auth.js';
 import { generarToken } from '../utils/tokenG.js';
 import { SendEmail } from '../emails/SendEmail.js';
-//import Database from '../bd/connection.js';
+import UserDAO from '../models/UserDAO.js';
 
-
+const instanciaUserConsult = new UserDAO();
 export class AuthController {
-
     static createAccount = async (email, pass) => {
         try {
             //un usuario no está activo si no está verificado
             //creando contraseña para prueba
             
             // Hash Password
-            pass = await hashPassword(pass);
+            const passHash = await hashPassword(pass);
 
             // Prevenir duplicados
 
             // Crea un usuario
             const usuarioPrueba = new User({
-                email: email,
-                password: pass,
-                active: false
+                correo_usuario: email,
+                password: passHash,
+                estado: 0,
+                token: " "
             });
 
             // Generar el token, el id debe ser consultado en la bd y traido acá
-            //Database.getInstance();
+            
             //c inserts y lo demás
-            const idUser = 48568;
+            
+            const userMat = await instanciaUserConsult.create(usuarioPrueba);
+            
             const token = new Token({
                 token: generarToken(),
-                userID: idUser,
-                createdAt: '2024-05-01 08:41:27',
+                userID: userMat.insertId,
+                createdAt: new Date().toISOString().replace('T', ' ').substring(0, 19),
             });
+            usuarioPrueba.token = token.token;
             // enviar el email
-            SendEmail.sendConfirmationEmail(usuarioPrueba, token);
             console.table(usuarioPrueba);
             console.table(token);
+            await SendEmail.sendConfirmationEmail(usuarioPrueba, token);
             console.log('Cuenta creada, revisa tu email para confirmarla');
+            
         } catch (error) {
             console.log('Hubo un error');
             console.log(error);
