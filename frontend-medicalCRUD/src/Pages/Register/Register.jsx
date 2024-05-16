f97db1b8a4e236a55d642c9d1a6fa734d2c2872c
Syztema <cristiansantiagotriana08@gmail.com>
2024-05-15 22:46:37 -0500

    Creacion de Modal para el Registro en Backend

--- frontend-medicalCRUD/src/Pages/Register/Register.jsx
+++ frontend-medicalCRUD/src/Pages/Register/Register.jsx
@@ -9,7 +9,84 @@ import { IoMdMail } from "react-icons/io";
 import { BsFillShieldLockFill } from "react-icons/bs";
 import { AiOutlineSwapRight } from "react-icons/ai";
 
+//IMPORT SWEET ALERT
+import Swal from "sweetalert2";
+import withReactContent from 'sweetalert2-react-content';
+
 function Register() {
+    
+  const showSwal = () => {
+    withReactContent(Swal).fire({
+      title: "Ingrese Sus Datos",
+      html: `
+      <strong><label>Nombres</label></strong>
+      <input class="swal2-input" id="nombres" type="text" placeholder="Digite sus Nombres" /><br />
+      <strong><label>Apellidos</label></strong>
+      <input class="swal2-input" id="apellidos" type="text" placeholder="Digite sus Apellidos" /><br />            
+      <strong><label>Nacimiento</label></strong>
+      <input class="swal2-input" id="fecha_naci" type="date" placeholder="Digite la Fecha" min="1914-01-01"/>
+      <br /> 
+      <strong><label>Lugar Nacimiento</label></strong>
+      <select class="swal2-input" id="lugar_naci">
+        <option value="-1">Seleccione una opción</option>
+        <option value="0">Bogota</option>
+        <option value="1">Medellin</option>
+        <option value="2">Cali</option>
+      </select>
+      <br/>
+      <strong><label>Direccion Residencia</label></strong>
+      <input class="swal2-input" id="direccion_re" type="text" placeholder="Digite la direccion de residencia" />
+      <br/>
+      <strong><label>Detalles Direccion</label></strong>
+      <input class="swal2-input" id="direccion_cor" type="text" placeholder="Digite alguna descripcion de su casa" />
+      <br/>
+      <strong><label>Estrato</label></strong>      
+      <input class="swal2-input" id="estrato" type="number" placeholder="Digite su Estrato" />
+      <strong><label>Ciudad Residencia</label></strong>
+      <select class="swal2-input" id="ciudad_resi">
+        <option value="-1">Seleccione una opción</option>
+        <option value="0">Bogota</option>
+        <option value="1">Medellin</option>
+        <option value="2">Cali</option>
+      </select>
+      <strong><label>Ciudad Afiliacion</label></strong>
+      <select class="swal2-input" id="ciudad_afili">
+        <option value="-1">Seleccione una opción</option>
+        <option value="0">Bogota</option>
+        <option value="1">Medellin</option>
+        <option value="2">Cali</option>
+      </select>
+          `,      
+      showCancelButton: true,
+      cancelButtonColor: 'grey',
+      confirmButtonText: 'Aceptar',
+      allowOutsideClick: false,
+      preConfirm: () => ({
+        nombres: document.getElementById('nombres').value,
+        apellidos: document.getElementById('apellidos').value,
+        fecha_naci: document.getElementById('fecha_naci').value,     
+        lugar_naci: document.getElementById('lugar_naci').value,      
+        direccion_re: document.getElementById('direccion_re').value,
+        direccion_cor: document.getElementById('direccion_cor').value,       
+        estrato: document.getElementById('estrato').value,      
+        ciudad_resi: document.getElementById('ciudad_resi').value,
+        ciudad_afili: document.getElementById('ciudad_afili').value
+      })
+    }).then((result) =>{
+      const { nombres, apellidos, fecha_naci, lugar_naci, direccion_re, direccion_cor, estrato, ciudad_resi, ciudad_afili} = result.value;
+      console.log("Nombres:", nombres);
+      console.log("Apellidos:", apellidos);
+      console.log("Nacimiento:", fecha_naci);
+      console.log("Lugar Naci:", lugar_naci);
+      console.log("Direccion Re:", direccion_re);
+      console.log("Detalles Direccion:", direccion_cor);
+      console.log("Estrato:", estrato);
+      console.log("Ciudad Resi:", ciudad_resi);
+      console.log("Ciudad Afili:", ciudad_afili);
+      window.location.href = "/register"; 
+    })
+  }
+
   return (
     <div className="registerPage flex">
       <div className="container flex">
@@ -25,14 +102,13 @@ function Register() {
               <button className="btn">Iniciar Sesion</button>
             </Link>
           </div>
-        </div>    
+        </div>
         <div className="formDiv flex">
           <div className="headerDiv">
             <img src={logo} alt="Logo Image" />
             <h3>Bienvenido a EPS Salud +</h3>
           </div>
-          <form action="" className="form grid">            
-            
+          <form action="" className="form grid">
             <div className="inputDiv">
               <label htmlFor="email">Correo</label>
               <div className="input flex">
@@ -67,18 +143,20 @@ function Register() {
                 />
               </div>
             </div>
-            <button type="submit" className="btn flex">
+
+            <button type="button" onClick={showSwal} className="btn flex">
               <span>Registrarme</span>
               <AiOutlineSwapRight className="icon" />
-            </button>
+            </button>                        
+
             <Link to={"/forgot"}>
               <span className="forgotPassword">
                 Olvidaste tu Contraseña? <a href="">Click Aqui</a>
               </span>
             </Link>
           </form>
-        </div>    
-      </div>      
+        </div>
+      </div>
     </div>
   );
 }
