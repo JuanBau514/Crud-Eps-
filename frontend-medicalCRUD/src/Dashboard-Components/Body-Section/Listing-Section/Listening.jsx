dded9712d90235e2cea5250536ee590491b41bd8
Juan Pablo Bautista <jbautistaclavijo@gmail.com>
2024-05-23 18:33:22 -0500

    no tengo internet en el trabajo.php :c

--- frontend-medicalCRUD/src/Dashboard-Components/Body-Section/Listing-Section/Listening.jsx
+++ frontend-medicalCRUD/src/Dashboard-Components/Body-Section/Listing-Section/Listening.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './listening.css'
 function Listening() {
     return (
-        <div>
+        <div className='listening'>
             Listening
         </div>
     )
